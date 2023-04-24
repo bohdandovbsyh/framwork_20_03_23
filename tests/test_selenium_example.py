5ee2ad30deb0c754f7e2ba91b82cca5e98514810
bohdan <dovbysh.bohdan@gmail.com>
2023-04-24 20:22:49 +0300

    Update test_selenium_example.py

--- tests/test_selenium_example.py
+++ tests/test_selenium_example.py
@@ -4,14 +4,7 @@ from utilities.config_reader import get_user_creds
 from utilities.web_ui.base_page import BasePage
 
 
-def test_login():
-    # login_page = open_login_page
-    # main_page = login_page.set_email(get_user_creds()[0]).set_password(get_user_creds()[1]).click_login_button()
-    # # assert main_page.is_navigation_bar_displayed(), 'Nav bar is not displayed'
-    # actual_text = main_page.get_start_accepting_orders_card_text()
-    is_visible = False
-    assert is_visible is False, 'Element is visible'
-    #
-    # assert actual_text == "Start accepting orders", 'Text not as expected'
-def test_number():
-    assert 1 == 1, 'Horray'
+def test_login(open_login_page):
+    login_page = open_login_page
+    main_page = login_page.set_email(get_user_creds()[0]).set_password(get_user_creds()[1]).click_login_button()
+    assert main_page.is_navigation_bar_displayed(), 'Nav bar is not displayed'
